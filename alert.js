ef1c7081c64a4c73a87a0bacbb683160939b409b
Bida24-101 <bida24-101@thuto.bac.ac.bw>
2025-05-28 21:31:28 +0200

    Create alert.js

---
+++ alert.js
@@ -0,0 +1,14 @@
+const btn = document.getElementById('alertBtn')
+ 
+btn.addEventListener('click', () => {
+    alert('We apologize for the inconvenience,this product is not available.')
+})
+
+
+
+
+
+
+
+
+
